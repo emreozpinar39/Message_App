56c00b30c728a8f8c42ee12c6afb62eb6ff93ead
Emre Özpinar <emre.ozpinar@bilgeadamboost.com>
2022-07-29 22:53:26 +0300

    Generic Repository Added

---
+++ Message_App/Core/Repositories/GenericRepository.cs
@@ -0,0 +1,52 @@
+﻿using Message_App.Core.IRepositories;
+using Message_App.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Message_App.Core.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected ApplicationDbContext _context;
+        protected DbSet<T> dbSet;
+        public GenericRepository(ApplicationDbContext context)
+        {
+            _context = context;
+            dbSet = context.Set<T>();
+        }
+        public virtual async Task<IEnumerable<T>> All()
+        {
+            return await dbSet.ToListAsync();
+        }
+
+        public virtual async Task<T> GetById(Guid id)
+        {
+            return await dbSet.FindAsync(id);
+        }
+
+        public virtual async Task<bool> Add(T entity)
+        {
+            await dbSet.AddAsync(entity);
+            return true;
+        }
+
+        public virtual Task<bool> Upsert(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual Task<bool> Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+
+
+
+   
+    }
+}
