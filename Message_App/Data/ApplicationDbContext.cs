07ccc518079a0fd0c33e46dce08e463a4b923d97
Emre Özpinar <emre.ozpinar@bilgeadamboost.com>
2022-07-29 22:14:13 +0300

    Db Connection added

--- Message_App/Data/ApplicationDbContext.cs
+++ Message_App/Data/ApplicationDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Message_App.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace Message_App.Data
 {
@@ -8,5 +10,30 @@ namespace Message_App.Data
         {
 
         }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-06N3HCH\\SQLEXPRESS;Database=UnitOfWorkDb;Trusted_Connection=True;");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Message>().HasOne(x => x.User).WithMany(y => y.Messages).HasForeignKey(z => z.UserId);
+
+            modelBuilder.Entity<Users>().HasData(new Users
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "admin",
+                LastName = "admin",
+                Email = "admin@admin.com",
+                Password = "admin"
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public virtual DbSet<Users> Users { get; set; }
+        public virtual DbSet<Message> Messages { get; set; }
     }
 }
