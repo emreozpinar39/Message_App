4fe15bcee9610904f463f65dc563bd8bc85218ad
Emre Özpinar <emre.ozpinar@bilgeadamboost.com>
2022-07-29 21:57:47 +0300

    Add project files.

---
+++ Message_App/Models/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Message_App.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public string MessageDate { get; set; } = DateTime.Now.ToString("dd/MM/yyyy");
+
+
+        public Guid UserId { get; set; }
+        public virtual Users User { get; set; }
+    }
+}
---
+++ Message_App/Models/Users.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Message_App.Models
+{
+    public class Users
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; } = "pass";
+        public string Email { get; set; }
+
+        public virtual ICollection<Message> Messages { get; set; }
+    }
+}
