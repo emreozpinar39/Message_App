93e84ef7d37e43c614b0897f634dece54ddef234
Emre Özpinar <emre.ozpinar@bilgeadamboost.com>
2022-08-28 16:38:50 +0300

    MessageController added with methods

---
+++ Message_App/Controllers/MessageController.cs
@@ -0,0 +1,85 @@
+﻿using Message_App.Core.IConfiguration;
+using Message_App.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace Message_App.Controllers
+{
+    [ApiController]
+    [Route("controller")]
+    public class MessageController : Controller
+    {
+        private readonly ILogger<MessageController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
+        public MessageController(ILogger<MessageController> logger, IUnitOfWork unitOfWork)
+        {
+            _logger = logger;
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllMessages()
+        {
+            var messages = await _unitOfWork.Messages.All();
+            return Ok(messages);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetMessage(int id)
+        {
+            var message = await _unitOfWork.Messages.GetByMessageId(id);
+
+            if (message == null)
+            {
+                return NotFound(); 
+            }
+            return Ok(message);
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddMessage(Message message)
+        {
+            if (ModelState.IsValid)
+            {
+
+                await _unitOfWork.Messages.Add(message);
+                await _unitOfWork.CompleteAsync();
+
+                return Ok(message);
+            }
+            return new JsonResult("Something went rong") { StatusCode = 500 };
+        }
+
+        [HttpPost("{id}")]
+        public async Task<IActionResult> UpdateMessage(int id, Message message)
+        {
+            if (id != message.Id)
+            {
+                return BadRequest();
+            }
+            await _unitOfWork.Messages.Update(message);
+            await _unitOfWork.CompleteAsync();
+
+            return Content("Message updated");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMessage(int id)
+        {
+            var message = await _unitOfWork.Messages.GetByMessageId(id);
+
+            if (message == null)
+            {
+                return BadRequest();
+            }
+
+            await _unitOfWork.Messages.DeleteMessage(id);
+            await _unitOfWork.CompleteAsync();
+
+            return NoContent();
+        }
+
+    }
+}
